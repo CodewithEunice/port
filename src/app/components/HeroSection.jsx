f1fdb955dcc068dc44a0be711c53a365b532c6b1
Eunice <eunice.itcosmed@gmail.com>
2024-12-18 22:05:52 +0100

    yea-yea

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -44,8 +44,8 @@ const HeroSection = () => {
               alt="hero image"
               className=" absolute pl-4 transform -translate-x-1/2 
                -translate-y-1/2 top-1/2 left-1/2"
-              width={300}
-              height={300}
+              width={400}
+              height={400}
             />
           </div>
         </motion.div>
