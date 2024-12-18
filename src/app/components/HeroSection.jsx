457f9cb1cbc63d74a7c32a0f1ac8689d0d53622f
Eunice <eunice.itcosmed@gmail.com>
2024-12-18 18:09:12 +0100

    complete-port

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -58,7 +58,7 @@ const HeroSection = () => {
             <Image
               src="/images/ai-2re.png"
               alt="hero image"
-              className=" absolute transform -translate-x-1/2 
+              className=" absolute hidden md:flex transform -translate-x-1/2 
                -translate-y-1/2 top-1/2 left-1/2"
               width={400}
               height={400}
