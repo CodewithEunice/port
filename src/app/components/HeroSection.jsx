4b900a501f41fd67e22603ea9855a336e2700800
Eunice <eunice.itcosmed@gmail.com>
2024-12-20 21:58:07 +0100

    final touch

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -16,20 +16,24 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className=" md: hidden lg:hidden text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              Hi {`I'm`} Eunice
+            </span>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
-              sequence={["Eunice", 1000, "a Web Developer", 1000]}
+              className=" sm:text-5xl md:text-7xl"
+              sequence={["Eunice,", 1000, "a Web Developer.", 1000]}
               wrapper="span"
               speed={50}
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            {`I'm `}a Front End Developer and I develop modern{" "}
-            <br className="sm:block hidden" /> web applications.
+          <p className="text-[#ADB7BE] sm:hidden md:flex text-base sm:text-lg mb-6 lg:text-xl">
+            {`I'm `}a Front End Developer and I specialize in <br /> developing
+            modern web applications.
           </p>
         </motion.div>
         <motion.div
@@ -38,7 +42,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="  w-[350px] h-[350px]  lg:w-[400px] lg:h-[400px] relative">
+          <div className="  w-[350px] h-[350px]  lg:w-[450px] lg:h-[450px] relative">
             <Image
               src="/images/ai-new.png"
               alt="hero image"
